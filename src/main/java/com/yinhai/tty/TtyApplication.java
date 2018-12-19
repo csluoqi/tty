39c30753153074c25862541db172dd5791556514
yanglei <18030652383@163.com>
2018-12-19 23:49:46 +0800

    11

--- src/main/java/com/yinhai/tty/TtyApplication.java
+++ src/main/java/com/yinhai/tty/TtyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TtyApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(TtyApplication.class, args);
 	}
